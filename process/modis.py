63e43efacf8f72fe0427ae1d27196c6e6ce68d69
Netherton <Kevin.Netherton@gov.bc.ca>
2021-04-08 19:58:24 -0700

    files not being created on object store

--- process/modis.py
+++ process/modis.py
@@ -102,8 +102,14 @@ def create_modis_mosaic(pth: str):
                       })
         # Write mosaic to disk
         out_pth = os.path.join(const.OUTPUT_TIF_MODIS,date.split('.')[0],f'{date}.tif')
+        logger.debug(f"out_pth: {out_pth}")
         try:
-            os.makedirs(os.path.split(out_pth)[0])
+            dir2Create = os.path.dirname(out_pth)
+            if not os.path.exists(dir2Create):
+                #os.makedirs(os.path.split(out_pth)[0])
+                os.makedirs(dir2Create)
+                logger.debug(f"created the dir: {dir2Create}")
+
         except Exception as e:
             logger.debug(e)
         with rio.open(out_pth, "w", **out_meta) as dst:
